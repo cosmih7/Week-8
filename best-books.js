8243493e8de284d90435b71873fc0035dd7554c3
Cosmin Hur <cosmih@uw.edu>
2025-12-01 20:29:04 -0800

    best book done

--- best-books.js
+++ best-books.js
@@ -2,6 +2,8 @@ const formEl = document.getElementById('best-books-form');
 const yearEl = document.getElementById('year');
 const monthEl = document.getElementById('month');
 const dateEl = document.getElementById('date');
+const booksContainer = document.getElementById("books-container") 
+
 
 formEl.addEventListener('submit', function(e) {
   e.preventDefault();
@@ -13,13 +15,25 @@ formEl.addEventListener('submit', function(e) {
   // Fetch bestselling books for date and add top 5 to page
   const URL = `https://api.nytimes.com/svc/books/v3/lists/${year}-${month}-${date}/hardcover-fiction.json?api-key=${API_KEY}`
 
-  fetch(URL).then((result) => {
-    return result.json()
-  }).then((data) => {
-    const books = data.results.books
+  fetch(URL)
+   .then((result) => {
+    return result.json();
+  })
+  .then((data) => {
+    const books = data.results.books;
 
     books.forEach((book) => {
-      console.log(book.title, book.author, book.description)
+     const newBook = document.createElement("div")
+     newBook.classList = "book"
+     const newBookHtml = `
+          <img src="${book.book_image}" />
+          <h3>${book.title}</h3>
+          <h5>${book.author}</h5>
+          <p>${book.description}</p>
+        `;
+
+        newBook.innerHTML = newBookHtml;
+        booksContainer.appendChild(newBook) 
     });
-  })
+  });
 });
